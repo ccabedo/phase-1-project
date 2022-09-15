f9359117061ad1dae52c9cda0137591617077d89
SiobhanMcCaffrey <siobhanamccaffrey@gmail.com>
2022-09-15 16:53:14 -0400

    thursday afternoon

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-fetch('https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
-.then(res => res.json())
-.then(dataList => {
-    console.log(dataList)
-
-dataList.forEach(products => {
-    renderProducts(products)
-    
-})
-})
-})
-
 const renderProducts = (products) => {
     let div = document.querySelector('.container')
-    let itemCard = document.createElement('div');
 
+    let itemCard = document.createElement('div');
     itemCard.className = 'item-card'
+
     let img = document.createElement('img')
     img.src = products.image_link
     
     let p = document.createElement('p')
     p.innerText = products.name
-    
 
+    let type = document.createElement('type')
+    type.innerText = products.product_type
+    
     let price = document.createElement('price')
     price.innerText = `$${products.price}`
     
-
     p.addEventListener('mouseover', () => {
         p.style.color = "pink";
     })
@@ -51,24 +39,52 @@ const renderProducts = (products) => {
     itemCard.append(img, p, price, button)
     div.appendChild(itemCard);
 
-  
+
 }
 
 let addToCart = document.querySelector(".sub")
 let cartTotal = 0;
 
-// const searchBar = document.getElementById('searchBar')
-
-// let searchBar = document.getElementById('searchBar')
-
-// searchBar.addEventListener('keyup', (e) => {
-//     const searchString = e.target.value;
-//     const filteredProducts = products.filter((products) => {
-//         products.name.includes(searchString) || 
-//         products.product_type.includes(searchString)
-//     })
-//     console.log(filteredProducts)
-// })
 
 
+document.addEventListener("DOMContentLoaded", function() {
+let products = []
+fetch('https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
+.then(res => res.json())
+.then(obj => {
+    console.log(obj)
+    obj.forEach(product => {
+        renderProducts(product)
+        products.push(product)
+        // console.log(product.name)
+    })
 
+
+
+
+
+
+let addToCart = document.querySelector(".sub")
+let cartTotal = 0;
+
+
+const searchBar = document.querySelector('#searchBar')
+const movieList = document.querySelector('.genreButton')
+const dataList = document.querySelector('.container')
+
+searchBar.addEventListener('input', (e) => {
+    console.log(e.target)
+    console.log('search', e.target.value)
+
+    dataList.innerHTML = ''
+
+    products.forEach(product => {
+
+        if (product.name.toLowerCase().includes(e.target.value.toLowerCase()) ) {
+            renderProducts(product)
+            console.log(product)
+        }
+    })
+})
+})
+})
